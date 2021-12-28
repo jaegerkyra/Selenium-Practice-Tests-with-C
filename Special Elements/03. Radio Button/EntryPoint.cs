f0f459ffcd8cdf60e2001b467540d790a6977627
Kyra Jaeger <87786202+jaegerkyra@users.noreply.github.com>
2021-12-27 23:00:52 -0600

    Added practice tests for radio buttons

--- Special Elements/03. Radio Button/EntryPoint.cs
+++ Special Elements/03. Radio Button/EntryPoint.cs
@@ -1,1 +1,41 @@
 
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+class EntryPoint
+{
+
+    static IWebDriver driver = new ChromeDriver();
+    static IWebElement radioButton;
+
+    static void Main()
+    {
+        //Variables
+        string url = "https://testing.todorvachev.com/radio-button-test/";
+        string[] option = { "1", "3", "5" };
+
+        //Navigation
+        driver.Navigate().GoToUrl(url);
+
+
+        for (int i = 0; i < option.Length; i++)
+        {
+            radioButton = driver.FindElement(By.CssSelector("#post-10 > div > form > p:nth-child(6) > input[type=radio]:nth-child(" + option[i] + ")"));
+
+            if (radioButton.GetAttribute("checked") == "true")
+            {
+                Console.WriteLine("The " + (i + 1) + " radio button is checked.");
+            }
+            else
+            {
+                Console.WriteLine("The " + (i + 1) + " radio button is one of the unchecked radio buttons.");
+            }
+        }
+
+        //PostCondition
+        driver.Quit();
+
+    }
+}
+
