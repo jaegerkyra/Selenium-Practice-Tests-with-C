5616b5ae68036124724ac9120b7716c316875531
Kyra Jaeger <87786202+jaegerkyra@users.noreply.github.com>
2021-12-27 23:02:09 -0600

    Added practice tests for alert boxes

--- Special Elements/05. Alert Box/EntryPoint.cs
+++ Special Elements/05. Alert Box/EntryPoint.cs
@@ -1,1 +1,52 @@
 
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Threading;
+
+class EntryPoint
+{
+
+    static IWebDriver driver = new ChromeDriver();
+    static IAlert alert;
+    static IWebElement image;
+
+    static void Main()
+    {
+        //Variables
+        string url = "https://testing.todorvachev.com/alert-box/";
+
+        //Navigation
+        driver.Navigate().GoToUrl(url);
+
+        alert = driver.SwitchTo().Alert();
+
+        Console.WriteLine(alert.Text);
+
+        alert.Accept();
+
+        image = driver.FindElement(By.CssSelector("#post-119 > div > figure > img"));
+
+        try
+        {
+            if (image.Displayed)
+            {
+                Console.WriteLine("The alert was successfully accepted and image is showing.");
+            }
+        }
+        catch (NoSuchElementException)
+        {
+            Console.WriteLine("Something went wrong");
+        }
+
+
+        Thread.Sleep(3000);
+
+
+        //PostCondition
+        driver.Quit();
+
+    }
+}
+
+
