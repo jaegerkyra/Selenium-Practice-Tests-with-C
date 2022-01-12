4ec3fa5d76dcb4b339f61622f22d2631d9b139e8
Kyra Jaeger <87786202+jaegerkyra@users.noreply.github.com>
2022-01-11 21:16:09 -0600

    Add files via upload

---
+++ AutoTestFramework/Scenarios/LoginInvalidUsername.cs
@@ -0,0 +1,53 @@
+﻿namespace AutoTestFramework.Pages
+{
+    using OpenQA.Selenium;
+    using NUnit.Framework;
+
+    [Parallelizable]
+    public class LoginInvalidUsername
+    {
+        IAlert alert;
+        public IWebDriver Driver { get; set; }
+
+        public LoginInvalidUsername()
+        {     
+        }
+
+        [OneTimeSetUp]
+        public void Initialize()
+        {
+            Driver = Actions.InitializeDriver();
+            NavigateTo.LoginFormScenarioThroughTestCases(Driver);
+        }
+
+        [TestCase]
+        public void LessThan5Chars()
+        {
+            NavigateTo.LoginFormScenarioThroughMenu(Driver);
+            Actions.FillLoginForm(Config.Credentials.Invalid.Username.FourCharacters, Config.Credentials.Valid.Password, Config.Credentials.Valid.Password, Driver);
+
+            alert = Driver.SwitchTo().Alert();
+
+            Assert.AreEqual(Config.AlertsTexts.UsernameLengthOutOfRange, alert.Text);
+            alert.Accept();            
+        }
+
+        [TestCase]
+        public void MoreThan12Chars()
+        {
+            Actions.FillLoginForm(Config.Credentials.Invalid.Username.ThirteenCharacters,
+                Config.Credentials.Valid.Password, Config.Credentials.Valid.Password, Driver);
+
+            alert = Driver.SwitchTo().Alert();
+
+            Assert.AreEqual(Config.AlertsTexts.UsernameLengthOutOfRange, alert.Text);
+            alert.Accept();
+        }
+       
+        [OneTimeTearDown]
+        public void CleanUp()
+        {
+            Driver.Quit();
+        }
+    }
+}
