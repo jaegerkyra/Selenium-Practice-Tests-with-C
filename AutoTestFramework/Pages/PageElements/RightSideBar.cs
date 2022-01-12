6ff1ac59324148bb625c42dd0e55fc0b18b1c6b7
Kyra Jaeger <87786202+jaegerkyra@users.noreply.github.com>
2022-01-11 21:12:51 -0600

    Add files via upload

---
+++ AutoTestFramework/Pages/PageElements/RightSideBar.cs
@@ -0,0 +1,23 @@
+﻿
+
+
+using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+
+namespace AutoTestFramework.UIElements
+{
+    class RightSideBar
+    {
+        public RightSideBar(IWebDriver driver)
+        {
+            PageFactory.InitElements(driver, this);
+        }
+
+        [FindsBy(How = How.CssSelector, Using = "recent-posts-2 > ul > li:nth-child(1)")]
+        public IWebElement FirstRecentPost { get; set; }
+
+        [FindsBy(How = How.Name, Using = "cat")]
+        public IWebElement CategoryMenu { get; set; }
+
+    }
+}
