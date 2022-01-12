e84f4f79528e5f6a6da7e8d46c5e3916b7d495ad
Kyra Jaeger <87786202+jaegerkyra@users.noreply.github.com>
2022-01-11 21:19:19 -0600

    Adding folder of Screenshot practice

---
+++ SeleniumScreenshot/EntryPoint.cs
@@ -0,0 +1,34 @@
+﻿
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.IO;
+
+namespace SeleniumScreenshot
+{
+    class EntryPoint
+    {
+        static void Main()
+        {
+            IWebDriver chrome = new ChromeDriver();
+
+            string screenshotsDirectory = Directory.GetCurrentDirectory() + @"\screenshots";
+
+            chrome.Navigate().GoToUrl("http://google.com");
+
+            Screenshot googleScreenshot = ((ITakesScreenshot)chrome).GetScreenshot();
+
+            if (!Directory.Exists(screenshotsDirectory))
+            {
+                Directory.CreateDirectory(Directory.GetCurrentDirectory() + @"\screenshots");
+            }
+
+            googleScreenshot.SaveAsFile(Directory.GetCurrentDirectory() + @"\screenshots\googlescreenshot.png", ScreenshotImageFormat.Png);
+
+            //If you wanted to see where exactly screenshots were being saved to be default
+            //System.Console.WriteLine(Directory.GetCurrentDirectory());
+
+
+            chrome.Quit();
+        }
+    }
+}
