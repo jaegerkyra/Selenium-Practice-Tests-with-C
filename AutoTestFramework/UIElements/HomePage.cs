08dc9cb762a3542baff34c1269eced0b065be5a9
Kyra Jaeger <87786202+jaegerkyra@users.noreply.github.com>
2022-01-11 21:06:09 -0600

    Update HomePage.cs

--- AutoTestFramework/UIElements/HomePage.cs
+++ AutoTestFramework/UIElements/HomePage.cs
@@ -1,1 +1,21 @@
 
+using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+
+namespace AutoTestFramework
+{
+    public class HomePage
+    {
+        public HomePage()
+        {
+            PageFactory.InitElements(Driver.driver, this);
+        }
+        //Elements
+
+        //Introduction Header
+        [FindsBy(How = How.CssSelector, Using = "#page-17 > header > h1")]
+
+        //Create property
+        public IWebElement Headline { get; set; }
+    }
+}
