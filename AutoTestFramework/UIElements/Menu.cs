8b79ff985607efcbc48a53e62bcf0fce160850ce
Kyra Jaeger <87786202+jaegerkyra@users.noreply.github.com>
2022-01-11 21:06:47 -0600

    Update Menu.cs

--- AutoTestFramework/UIElements/Menu.cs
+++ AutoTestFramework/UIElements/Menu.cs
@@ -1,1 +1,39 @@
 
+using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+
+namespace AutoTestFramework
+{
+    public class Menu
+    {
+        public Menu()
+        {
+            PageFactory.InitElements(Driver.driver, this);
+        }
+        //Elements
+
+        //Introduction Menu
+        [FindsBy(How = How.Id, Using = "menu-item-25")]
+        public IWebElement Introduction { get; set; }
+
+        //Selectors Menu
+        [FindsBy(How = How.Id, Using = "menu-item-106")]
+        public IWebElement Selectors { get; set; }
+
+        //Special Elements Menu
+        [FindsBy(How = How.Id, Using = "menu-item-35")]
+        public IWebElement SpecialElements { get; set; }
+
+        //Test Cases Menu
+        [FindsBy(How = How.Id, Using = "menu-item-57")]
+        public IWebElement TestCases { get; set; }
+
+        //Test Sceanarios Menu
+        [FindsBy(How = How.Id, Using = "menu-item-28")]
+        public IWebElement TestScenarios { get; set; }
+
+        //About Menu
+        [FindsBy(How = How.Id, Using = "menu-item-26")]
+        public IWebElement About { get; set; }
+    }
+}
